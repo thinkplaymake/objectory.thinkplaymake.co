ff62b42f2bff75108b84643e9e6b0640d9ccf16d
Matthew Knight <matthew@thinkplaymake.co>
2012-07-02 16:38:18 +0100

    Added test flag statement to local config.

--- public/htdocs/api/v1/_localconfig.default.php
+++ public/htdocs/api/v1/_localconfig.default.php
@@ -1,4 +1,8 @@
 <?php
 
 	// rename this file to _localconfig.php
-	define( 'MONGO_DB', 'ENTER_YOUR_MONGODB_CONNECTION_STRING_HERE' );
+	
+	
+	
+	define( 'MONGO_DB', 'ENTER_YOUR_MONGODB_CONNECTION_STRING_HERE' );  // your mongodb connection string
+	define( 'OBJECTORY_TESTDATAFLAG', true );							// whether your data should be posted as 'test' (useful for setup/debugging)
